d54eb4cce2f32879d767b0e279d1193bdd7588a4
Emyr Williams <egwilliams2002@googlemail.com>
2020-06-19 17:27:24 +0100

    Added a person code, and refactored the main a wee bit

--- src/main/java/MyMongoClient.java
+++ src/main/java/MyMongoClient.java
@@ -6,9 +6,24 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyMongoClient {
+
+    /**
+     * @return Document generated from Person object.
+     */
+    public static Document personToDocument(Person person) {
+        return new Document("id", person.getID())
+                .append("name", person.getName())
+                .append("Address", person.getContactDetails());
+    }
+
+    public Document getPerson(String searchTerm) {
+
+    }
 
     public static void main(String args[]) {
         // MongoClient is essentially our connection pool to the
@@ -21,9 +36,9 @@ public class MyMongoClient {
         // This isn't the code in the tutorial.
         MongoCollection<Document> myCollection = database.getCollection("myCollection");
 
-        // Build an object to put in our DB.
-        List<Integer> bookISBNS = Arrays.asList(12312,1235,32423,1213123);
+        List<Integer> bookISBNS = Arrays.asList(1234, 4567, 6789);
 
+        // One way of putting data in to the collection.
         Document person = new Document("_id", "jo")
                 .append("name", "Jonathon Finbar Saunders")
                 .append("address", new BasicDBObject("street", "123 Somerset Street")
@@ -33,6 +48,18 @@ public class MyMongoClient {
                 .append("books", bookISBNS);
 
         myCollection.insertOne(person);
+
+
+        // However, we'll normally build an object, serialize it in to a Document object
+        // and then pump that to the Database. But I'm aware that I'm barely scratching the
+        // surface here.
+        Map<String, String> addressMap = new HashMap<>();
+        addressMap.put("AddressLine1", "Jedi Temple");
+        addressMap.put("AddressLine2", "Senatorial District");
+        addressMap.put("AddressLine3", "Coruscant");
+
+        Person p1 = new Person("Obi-Wan", "Obi-Wan Kenobi", addressMap);
+        myCollection.insertOne(personToDocument(p1));
 
         // we should ALWAYS close our mongo client when we shut down.
         mc.close();
---
+++ src/main/java/Person.java
@@ -0,0 +1,46 @@
+
+import java.util.Map;
+
+public class Person {
+    private String id;
+    private String name;
+    private Map<String, String> contactDetails;
+
+    public Person(String id, String name, Map<String, String> ct) {
+        this.id = id;
+        this.name = name;
+        this.contactDetails = ct;
+    }
+
+    /**
+     * Get the map containing the contact details.
+     * @return
+     */
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Parses the contactDetails map, to print a formatted address
+     * to the screen.
+     * @return String - Formatted address.
+     */
+    public String printFormattedAddress() {
+        StringBuilder addressString = new StringBuilder();
+        for(Map.Entry<String, String> entries : contactDetails.entrySet()) {
+            addressString.append(entries.getKey());
+            addressString.append(": \t\t");
+            addressString.append(entries.getValue());
+            addressString.append("\n" );
+        }
+        return addressString.toString();
+    }
+}
