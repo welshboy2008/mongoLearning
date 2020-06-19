0593e50fd233594718b12e8f4dc2fe04694bec14
Emyr Williams <egwilliams2002@googlemail.com>
2020-06-19 17:50:01 +0100

    Refactored the client code slightly to be neater, and allow me to change some stuff later

--- src/main/java/MyMongoClient.java
+++ src/main/java/MyMongoClient.java
@@ -1,8 +1,8 @@
 import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
 
 import java.util.Arrays;
@@ -10,32 +10,64 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.mongodb.client.model.Filters.eq;
+
 public class MyMongoClient {
+    private static MongoClient mc;
+    private static MongoDatabase database;
+    private static MongoCollection<Document> myCollection;
+
+    /**
+     * Set up the various variables for us to access the
+     * Mongo collections.
+     */
+    public MyMongoClient() {
+        mc = new MongoClient();
+        database = mc.getDatabase("myDatabase");
+        myCollection = database.getCollection("myCollection");
+    }
 
     /**
      * @return Document generated from Person object.
      */
     public static Document personToDocument(Person person) {
-        return new Document("id", person.getID())
+        return new Document("_id", person.getID())
                 .append("name", person.getName())
                 .append("Address", person.getContactDetails());
     }
 
-    public Document getPerson(String searchTerm) {
+    /**
+     * Look for a record within the database with a specific _id
+     * @param searchTerm - the ID of the record in the DB
+     * @return Document if found, null if not.
+     */
+    public Document getPersonByID(String searchTerm) {
+        return myCollection.find(eq("_id", searchTerm)).first();
+    }
 
+    public void addRecord(Document record) {
+        myCollection.insertOne(record);
+    }
+
+    public void addObject(Person person) {
+        myCollection.insertOne(personToDocument(person));
+    }
+
+    public void closeDatabase() {
+        mc.close();
+    }
+
+    public void wipeDatabase() {
+        mc.dropDatabase("myCollection");
+    }
+
+    public void removeRecord(String searchTerm) {
+        DeleteResult dr = myCollection.deleteOne(eq("_id", searchTerm));
+        System.out.println("Number of records deleted: " + dr.getDeletedCount());
     }
 
     public static void main(String args[]) {
-        // MongoClient is essentially our connection pool to the
-        // document database.
-        MongoClient mc = new MongoClient();
-
-        // This data base if it doesn't exist, will be created when we put something in it.
-        MongoDatabase database = mc.getDatabase("myDatabase");
-
-        // This isn't the code in the tutorial.
-        MongoCollection<Document> myCollection = database.getCollection("myCollection");
-
+        MyMongoClient myc = new MyMongoClient();
         List<Integer> bookISBNS = Arrays.asList(1234, 4567, 6789);
 
         // One way of putting data in to the collection.
@@ -46,9 +78,7 @@ public class MyMongoClient {
                         .append("county", "someCounty")
                         .append("post-code", "SS12 3GH"))
                 .append("books", bookISBNS);
-
-        myCollection.insertOne(person);
-
+        myc.addRecord(person);
 
         // However, we'll normally build an object, serialize it in to a Document object
         // and then pump that to the Database. But I'm aware that I'm barely scratching the
@@ -59,9 +89,11 @@ public class MyMongoClient {
         addressMap.put("AddressLine3", "Coruscant");
 
         Person p1 = new Person("Obi-Wan", "Obi-Wan Kenobi", addressMap);
-        myCollection.insertOne(personToDocument(p1));
+        myc.addObject(p1);
+        myc.removeRecord("jo");
 
         // we should ALWAYS close our mongo client when we shut down.
-        mc.close();
+        //myc.wipeDatabase();
+        myc.closeDatabase();
     }
 }
